d5c5b2b565d0d3e234d4a04d97b99f571440011e
hideki.shima@gmail.com <hideki.shima@gmail.com@755b6b0a-766d-1655-8634-96f16c0b9a82>
2013-04-10 21:36:28 +0000

    showing measures

--- branches/edu.cmu.lti.ws4j.web/src/edu/cmu/lti/ws4j/web/DemoServlet.java
+++ branches/edu.cmu.lti.ws4j.web/src/edu/cmu/lti/ws4j/web/DemoServlet.java
@@ -21,6 +21,7 @@ import edu.cmu.lti.ram_wordnet.OnMemoryWordNetAPI;
 import edu.cmu.lti.ws4j.Relatedness;
 import edu.cmu.lti.ws4j.RelatednessCalculator;
 import edu.cmu.lti.ws4j.RelatednessCalculator.Parameters;
+import edu.cmu.lti.ws4j.data.Measure;
 import edu.cmu.lti.ws4j.impl.HirstStOnge;
 import edu.cmu.lti.ws4j.impl.JiangConrath;
 import edu.cmu.lti.ws4j.impl.LeacockChodorow;
@@ -28,6 +29,7 @@ import edu.cmu.lti.ws4j.impl.Lesk;
 import edu.cmu.lti.ws4j.impl.Lin;
 import edu.cmu.lti.ws4j.impl.Resnik;
 import edu.cmu.lti.ws4j.impl.WuPalmer;
+import edu.cmu.lti.ws4j.util.WS4JConfiguration;
 import edu.washington.cs.knowitall.morpha.MorphaStemmer;
 
 @SuppressWarnings("serial")
@@ -43,7 +45,7 @@ public class DemoServlet extends HttpServlet {
   }
   
   private AbstractWordNet wn;
-  private Map<String,RelatednessCalculator> rcs;
+  private Map<Measure,RelatednessCalculator> rcs;
 //  private KStemmer stemmer;
   
   @Override
@@ -51,7 +53,7 @@ public class DemoServlet extends HttpServlet {
           throws IOException {
     res.setContentType("text/html");
     res.getWriter().println( getHeader() );
-    res.getWriter().println( "<h1>WS4J Demo</h1>\n" +
+    res.getWriter().println( "<h1><a href=\"/\" style=\"text-decoration:none;color:#333333\">WS4J Demo</a></h1>\n" +
     		"WS4J (WordNet Similarity for Java) measures semantic similarity/relatedness between words.<br><br>\n" );
 
     String w1 = req.getParameter("w1");
@@ -59,7 +61,11 @@ public class DemoServlet extends HttpServlet {
     String measure = req.getParameter("measure");
     String s1 = req.getParameter("s1");
     String s2 = req.getParameter("s2");
-    if ( w1!=null && w2!=null ) {
+    String mode = req.getParameter("mode");
+    boolean wordMode = mode!=null && mode.equals("w") && w1!=null && w2!=null;
+    
+    showForm(res.getWriter(), s1, s2, w1, w2, measure, wordMode);
+    if ( wordMode ) {
       runOnWords( res.getWriter(), w1, w2, measure );
     } else {//default or when s1 & s1 exist
       runOnSentences( res.getWriter(), s1, s2 );
@@ -68,39 +74,62 @@ public class DemoServlet extends HttpServlet {
     res.getWriter().println( "<hr><a href=\"https://code.google.com/p/ws4j/\">WS4J</a> demo is maintained by <a href=\"http://www.cs.cmu.edu/~hideki\">Hideki Shima</a>.\n" );
     res.getWriter().println( "</body>\n</html>\n" );
   }
+  
+  private void showForm(PrintWriter out, String s1, String s2, 
+          String w1, String w2, String measure, boolean wordMode) {
+    StringBuilder sbForm = new StringBuilder();
+    sbForm.append("<form action=\"/\" method=\"get\" onsubmit=\"return validate()\">\n");
+    sbForm.append("Type in texts below, or use:\n");
+    sbForm.append("&nbsp;<input type='button' value='  example sentences  ' onclick='insert_sampleS()'>&nbsp;\n");
+    sbForm.append("&nbsp;<input type='button' value='  example words  ' onclick='insert_sampleW()'><br><br>\n");
+    sbForm.append("<table class='form'>\n");
+    sbForm.append("<tr><td class='th1'>1. </td><td class='th1'>Input mode</td><td>\n");
+    sbForm.append("<input type='radio' name='mode' value='s' id='s_mode' onclick='sentMode()' "+(wordMode?"":"checked")+"><label for='s_mode'>Sentence</label>&nbsp;&nbsp;&nbsp;\n");
+    sbForm.append("<input type='radio' name='mode' value='w' id='w_mode' onclick='wordMode()' "+(wordMode?"checked":"")+"><label for='w_mode' title='format: \"dog\" or \"dog#n\" or \"dog#n#1\"'>Word</label></td></tr>\n");
+    sbForm.append("<tr><td>2. </td><td><span class='mode_label'>Sentence</span> 1</td><td>\n");
+    sbForm.append("<div id='s1wrapper' style='display:"+(wordMode?"none":"block")+"'><textarea rows='4' cols='40' id='s1' name='s1' placeholder='the first sentence goes here'>"+(s1==null?"":s1)+"</textarea></div>\n");
+    sbForm.append("<div id='w1wrapper' style='display:"+(wordMode?"block":"none")+"'><input id='w1' name='w1' placeholder='the first word' value='"+(w1==null?"":w1)+"'></div></td></tr>\n");
+    sbForm.append("<tr><td>3. </td><td><span class='mode_label'>Sentence</span> 2</td><td>\n");
+    sbForm.append("<div id='s2wrapper' style='display:"+(wordMode?"none":"block")+"'><textarea rows='4' cols='40' id='s2' name='s2' placeholder='the second sentence goes here'>"+(s2==null?"":s2)+"</textarea></div>\n");
+    sbForm.append("<div id='w2wrapper' style='display:"+(wordMode?"block":"none")+"'><input id='w2' name='w2' placeholder='the second word' value='"+(w2==null?"":w2)+"'></div></td></tr>\n");
+    sbForm.append("<tr><td>4. </td><td>Submit</td><td><div style='text-align:center'><input type='submit' value='       Calculate Semantic Similarity       '></div></td></tr>\n");
+    sbForm.append("</table></form>\n");
+    out.println( sbForm );
+  }
 
   public void runOnWords( PrintWriter out, String w1, String w2, String measure ) {
 //    WS4JConfiguration.getInstance().setTrace(true);//doesn't work here
 //    long t00 = System.currentTimeMillis();
-    List<String> measures;
+    List<Measure> measures;
     if (measure!=null) {
-      measures = new ArrayList<String>();
-      measures.add(measure);
+      measures = new ArrayList<Measure>();
+      measures.add(Measure.valueOf(measure.toUpperCase()));
     } else {
-      measures = new ArrayList<String>(rcs.keySet());
+      measures = new ArrayList<Measure>(rcs.keySet());
     }
     RelatednessCalculator.enableTrace = true;
-    for ( String m : measures ) {
+    for ( Measure m : measures ) {
       RelatednessCalculator rc = rcs.get(m);
 //      long t0 = System.currentTimeMillis();
       StringBuilder sbResult = new StringBuilder();
       sbResult.append( "<h2>"+m+"</h2>\n" );
-      if (m.equals("LESK")) {
+      if (m==Measure.LESK) {
         sbResult.append(m+" demo is coming soon.");
-      } else {
+      } else { 
         Relatedness r = rc.calcRelatednessOfWords(w1, w2, true);
-        String log = r.getTrace().replaceAll("<","&lt;").replaceAll("\n", "<br>\n");
+        String log = r.getTrace().replaceAll(" < "," &lt; ").replaceAll("\n", "<br>\n");
         StringBuffer sb = new StringBuffer();
         Pattern patter = Pattern.compile("((^|\\n)[a-z]{3}\\(.+?\\) = [0-9.Ee-]+)");
         Matcher matcher = patter.matcher(log);
         while ( matcher.find() ) {
-          matcher.appendReplacement(sb, "<h3>"+matcher.group(1)+"</h3>");
+          matcher.appendReplacement(sb, "<h3>"+matcher.group(1)+"</h3>\n");
         }
         matcher.appendTail(sb);
         sbResult.append(sb.toString());
 //        long t1 = System.currentTimeMillis();
 //        sbResult.append("<textarea rows=\"6\" cols=\"70\">");
-        sbResult.append("<br><h3>Parameters</h3><ul>\n");
+        sbResult.append("<br><h3>Description of "+m+"</h3>\n"+m.getDescription().replaceAll("\n", "<br>\n")+"\n\n");
+        sbResult.append("<br><h3>Parameters</h3>\n<ul>\n");
         Parameters param = rc.dumpParameters();
         for ( String key : param.keySet() ) sbResult.append( "<li>"+key+" = "+param.get(key)+"</li>\n" );  
         sbResult.append("</ul>");
@@ -114,29 +143,18 @@ public class DemoServlet extends HttpServlet {
   }
   
   public void runOnSentences( PrintWriter out, String s1, String s2 ) {
-    StringBuilder sbForm = new StringBuilder();
-    sbForm.append("<form action=\"/\" method=\"get\" onsubmit=\"return validate()\">\n");
-    sbForm.append("Type in a pair of words or sentences to compare similarity, or use example sentences by clicking on:\n");
-    sbForm.append("&nbsp;<input type=\"button\" value=\"  insert sample sentences  \" onclick=\"insert_sample()\"><br><br>\n");
-    sbForm.append("&nbsp;<textarea rows=\"4\" cols=\"40\" id=\"s1\" name=\"s1\" placeholder=\"the first sentence goes here\">"+(s1==null?"":s1)+"</textarea><br>\n");
-    sbForm.append("&nbsp;<textarea rows=\"4\" cols=\"40\" id=\"s2\" name=\"s2\" placeholder=\"the second sentence goes here\">"+(s2==null?"":s2)+"</textarea><br>\n");
-    sbForm.append("&nbsp;<input type=\"submit\" value=\"  Calculate  \"><br>\n");
-    sbForm.append("</form>\n");
     if (s1==null || s2==null) {
-      out.println( sbForm );
       return;
     }
-    
-    sbForm.append("[Tips] In this demo, the following preprocessing are done before WS4J: tokenization, POS tagging, lemmatization.<br>\n" +
-        "If you see unexpected results due to errors in preprocessing, simply type in a list of lemmatized words.<br>\n");
-    out.println( sbForm );
-    
+//    sbForm.append("[Tips] In this demo, the following preprocessing are done before WS4J: tokenization, POS tagging, lemmatization.<br>\n" +
+//        "If you see unexpected results due to errors in preprocessing, simply type in a list of lemmatized words.<br>\n");
+
     String[] words1 = OpenNLPSingleton.INSTANCE.tokenize(s1);
     String[] words2 = OpenNLPSingleton.INSTANCE.tokenize(s2);
     String[] postag1 = OpenNLPSingleton.INSTANCE.postag(words1);
     String[] postag2 = OpenNLPSingleton.INSTANCE.postag(words2);
     long t00 = System.currentTimeMillis();
-    for ( String m : rcs.keySet() ) {
+    for ( Measure m : rcs.keySet() ) {
       RelatednessCalculator rc = rcs.get(m);
       long t0 = System.currentTimeMillis();
       StringBuilder sbResult = new StringBuilder();
@@ -144,7 +162,7 @@ public class DemoServlet extends HttpServlet {
       if (m.equals("LESK")) {
         sbResult.append(m+" demo is coming soon.");
       } else {
-        sbResult.append("<table border=1><tr><td class=\"th\">&nbsp;</td>");
+        sbResult.append("<table border=1 class=\"data\"><tr><td class=\"th\">&nbsp;</td>");
         for ( int i=0; i<words1.length; i++ ) {
           sbResult.append("<td class=\"th\">"+words1[i]+"<br><span class=\"g\">/"+postag1[i]+"</span></td>\n");
         }
@@ -173,7 +191,7 @@ public class DemoServlet extends HttpServlet {
               popup = m+"('"+(r.getSynset1()==null?w1+"#"+p1:r.getSynset1())+"', '"+(r.getSynset2()==null?w2+"#"+p2:r.getSynset2())+"')";
             }
             String dText;
-            String url = "ws4j?w1="+w1+"&w2="+w2+"&measure="+m;
+            String url = "ws4j?w1="+w1+"&w2="+w2+"&measure="+m+"&mode=w";
             if ( d <= 0 ) {
               if (error) {
                 dText = "<span class=\"g\" title=\""+popup+" = "+errorMsg+"\">-</span>";
@@ -196,7 +214,9 @@ public class DemoServlet extends HttpServlet {
         long t1 = System.currentTimeMillis();
 //        sbResult.append("<div title=\"tokenization, pos tagging, stemming, semantic similarity calculation, table generation\">\n</div>");
         sbResult.append("<textarea rows=\"6\" cols=\"70\">");
-        sbResult.append("Done in "+(t1-t0)+" msec.\n\nParameters:\n");
+        sbResult.append("Done in "+(t1-t0)+" msec.\n\n");
+        sbResult.append(m+": "+m.getDescription()+"\n\n");
+        sbResult.append("Parameters:\n");
         Parameters p = rc.dumpParameters();
         for ( String key : p.keySet() ) sbResult.append( " - "+key+" = "+p.get(key)+"\n" );  
         sbResult.append("</textarea>");
@@ -218,15 +238,17 @@ public class DemoServlet extends HttpServlet {
     long t0 = System.currentTimeMillis();
     try {
 //      stemmer = new KStemmer();
+      WS4JConfiguration.getInstance().setMFS(false);
+      WS4JConfiguration.getInstance().setLeskNormalize(false);
       wn = new OnMemoryWordNetAPI();
-      rcs = new LinkedHashMap<String,RelatednessCalculator>();
-      rcs.put("WUP",  new WuPalmer(wn));
-      rcs.put("RES",  new Resnik(wn));
-      rcs.put("JCN",  new JiangConrath(wn));
-      rcs.put("LIN",  new Lin(wn));
-      rcs.put("LCH",  new LeacockChodorow(wn));
-      rcs.put("HSO",  new HirstStOnge(wn));
-      rcs.put("LESK", new Lesk(wn));
+      rcs = new LinkedHashMap<Measure,RelatednessCalculator>();
+      rcs.put(Measure.WUP,  new WuPalmer(wn));
+      rcs.put(Measure.RES,  new Resnik(wn));
+      rcs.put(Measure.JCN,  new JiangConrath(wn));
+      rcs.put(Measure.LIN,  new Lin(wn));
+      rcs.put(Measure.LCH,  new LeacockChodorow(wn));
+      rcs.put(Measure.HSO,  new HirstStOnge(wn));
+      rcs.put(Measure.LESK, new Lesk(wn));
 //      SQL.getInstance();
       OpenNLPSingleton.INSTANCE.tokenize("");
     } catch (Exception e) {
@@ -249,33 +271,10 @@ public class DemoServlet extends HttpServlet {
     sb.append("<html>\n");
     sb.append("<head>\n");
     sb.append("<title>WS4J Demo</title>\n");
-    sb.append("<!--<link rel=\"alternate\" type=\"application/rss+xml\" title=\"RSS\" href=\"\" />-->\n");
     sb.append("<meta charset=\"utf-8\" />\n");
-    sb.append("<style>\n");
-    sb.append("body,td {\n");
-    sb.append("  font-size: 11px;\n");
-    sb.append("  font-family: 'ヒラギノ角ゴ Pro W3','Hiragino Kaku Gothic Pro','メイリオ',Meiryo,'ＭＳ Ｐゴシック',sans-serif;\n");
-    sb.append("}\n");
-    sb.append("h1 { font-weight: bold; font-size: 24px; margin-bottom: 0px; }\n");
-    sb.append("h2 { font-weight: bold; font-size: 20px; border-bottom: 1px solid #000000; margin-top: 30px; }\n");
-    sb.append("h3 { font-size: 14px; }\n");
-    sb.append("table { border:1px solid #666; border-collapse: collapse; border-spacing: 0px; }\n");
-    sb.append("td { margin:0px; text-align:right; font-size:11px; border:1px solid #666; border-collapse: collapse; border-spacing: 0px; }\n");
-    sb.append("td.th { padding-left:5px; padding-right:5px; font-size:10px; font-weight: bold; text-align:left; padding-left:2px; background-color:rgb(255, 255, 173) }\n");
-    sb.append(".g { color:#666666; }\n");
-    sb.append("</style>\n");
-    sb.append("<script>\n");
-    sb.append("function insert_sample() {\n");
-    sb.append("  document.getElementById('s1').value = \""+sample1+"\";\n");
-    sb.append("  document.getElementById('s2').value = \""+sample2+"\";\n");
-    sb.append("}\n");
-    sb.append("function validate() {\n");
-    sb.append("  var s1 = document.getElementById('s1').value;\n");
-    sb.append("  var s2 = document.getElementById('s2').value;\n");
-    sb.append("  if (s1.trim().length==0||s2.trim().length==0) {alert(\"Please fill in both of the text boxes.\"); return false;}\n");
-    sb.append("  return s1.length * s2.length <= 10000 || confirm(\"Input texts are long so it may take some time. Is it ok?\");\n");
-    sb.append("}\n");    
-    sb.append("</script>\n");
+    sb.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"/demo.css\">\n");
+    sb.append("<script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js\"></script>\n");
+    sb.append("<script type=\"text/javascript\" src=\"/demo.js\"></script>\n");
     sb.append("</head>\n");
     sb.append("<body>\n");
     return sb.toString();
