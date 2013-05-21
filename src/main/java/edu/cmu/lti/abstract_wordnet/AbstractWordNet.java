c70777ed6e3caf5b1dac251c34e99b19eb82b513
hideki.shima@gmail.com <hideki.shima@gmail.com@755b6b0a-766d-1655-8634-96f16c0b9a82>
2013-05-21 00:46:00 +0000

    links obtained from wn in batch rather than individual

--- src/main/java/edu/cmu/lti/abstract_wordnet/AbstractWordNet.java
+++ src/main/java/edu/cmu/lti/abstract_wordnet/AbstractWordNet.java
@@ -16,12 +16,17 @@
 package edu.cmu.lti.abstract_wordnet;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public abstract class AbstractWordNet {
 
+  protected final static String msgInvalidPOS   = "Invalid POS";
+  protected final static String msgInvalidSense = "Invalid Sense Number";
+  protected final static String msgInvalidWord  = "Word not found in WordNet";
+  
   /**
    * Given word and pos, find the synsets associated with them.
    * @param wordLemma
@@ -30,15 +35,6 @@ public abstract class AbstractWordNet {
    */
   public abstract List<Synset> getSynsets(String wordLemma, POS pos);
   
-//  public abstract List<String> getSynsetsByWordLemma( String wordLemma );
-
-  public Synset getSynset( String wordLemma, POS pos, int senseId ) {
-    List<Synset> synsets = getSynsets(wordLemma, pos);
-    if ( synsets==null ) return null;
-    if ( synsets.size() < senseId ) return null;
-    return synsets.get(senseId-1);
-  }
-
   /**
    * Get word lemmas given synset.
    * 
@@ -60,8 +56,9 @@ public abstract class AbstractWordNet {
    * @param link
    * @return glosses or empty collection if N/A
    */
-  public abstract List<Synset> getLinkedSynsets(Synset synset, Link link);
-  
+  public abstract Map<Link,List<Synset>> getLinkedSynsets(Synset synset, List<Link> links);
+//public abstract Map<Link,List<Synset>> getLinkedSynsets(Synset synset);
+
   /**
    * Given a synset s, word w and link l, first find synsets and words
    * that are connected to s with a link relation l.
@@ -77,7 +74,8 @@ public abstract class AbstractWordNet {
    * @param link
    * @return glosses or empty collection if N/A
    */
-  public abstract List<Synset> getLinkedWords(String synsetId, String word, Link link);
+   public abstract Map<Link,List<Synset>> getLinkedWords(String synsetId, String word, List<Link> links);
+//    public abstract Map<Link,List<Synset>> getLinkedWords(String synsetId, String word);
 
   /**
    * Given a synset s, find gloss (dictionary definition) of s  
@@ -86,6 +84,15 @@ public abstract class AbstractWordNet {
    * @return gloss
    */
   public abstract String getGloss( String synsetId );
+
+  /**
+   * (Optional method to implement)
+   *  
+   * Used for input completion/suggestion.
+   *  
+   * @return all words (lemmas) available for look up in WordNet
+   */
+  public abstract Set<String> dumpWords();
 
   /**
    * Given word and pos, find the most frequent synset
@@ -99,108 +106,92 @@ public abstract class AbstractWordNet {
     return result.size()>0 ? result.get(0) : null;
   }
 
-  /**
-   * Given a synset id s, find synset(s) of  
-   * direct horizontal term(s) of s.
-   * 
-   * Such terms are linked with relations such as: ants, attr, sim.
-   * 
-   * @param synsetId
-   * @return synsets of direct hypernym or empty collection if N/A
-   */
-  public List<Synset> getHorizontals(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.also, Link.attr, Link.sim, 
-            Link.ants, Link.pert} );
-  }
+//  private final static boolean lenient = true;
   
-  /**
-   * Given a synset id s, find synset(s) of  
-   * direct upward term(s) of s.
-   * 
-   * Such terms are linked with relations such as: 
-   * hype, mero, mmem, mprt, msub
-   * 
-   * Used by HSO algorithm.
-   * 
-   * @param synset
-   * @return synsets of direct hypernym or empty collection if N/A
-   */
-  public List<Synset> getUpwards(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.hype, Link.inst,//hypes 
-            Link.mmem, Link.msub,Link.mprt } );//mero 
-  }
+//  public Map<Link,List<Synset>> getLinkedSynsetsAndWords( Synset synset ) {
+//    Map<Link,List<Synset>> retval = new LinkedHashMap<Link,List<Synset>>(Link.values().length*4/3+1);
+//    retval.putAll( getLinkedSynsets( synset ) );
+//    retval.putAll( getLinkedWords( synset.getSynsetId(), synset.getWord() ) );
+//    
+//    Set<String> history = new HashSet<String>();
+//    List<Synset> duplicated = new ArrayList<Synset>();
+//    for ( Synset s : retval ) {
+//      if (history.contains(s.getSynsetId())) {
+//        duplicated.add(s);
+//      } else {
+//        history.add(s.getSynsetId());
+//      }
+//    }
+//    retval.removeAll(duplicated);
+//    return retval;
+//  }
 
-  /**
-   * Given a synset id s, find synset(s) of  
-   * direct downward term(s) of s.
-   * 
-   * Such terms are linked with relations such as: 
-   * caus, enta, holo, hmem, hsub, hprt, hypo.
-   * 
-   * Used by HSO algorithm.
-   * 
-   * @param synset
-   * @return synsets of direct hypernym or empty collection if N/A
-   */
-  public List<Synset> getDownwards(Synset synset) {
-    return getLinkedSynsets( synset,
-            new Link[]{Link.hmem, Link.hsub, Link.hprt,//holo 
-            Link.hypo, Link.hasi, //hypos
-            Link.caus, Link.enta, } );//etc
-  }
-  
-  public List<Synset> getHypes(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.hype, Link.inst} );
-  }
-  public List<Synset> getHypos(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.hypo, Link.hasi} );
-  }
-  public List<Synset> getAllMeronyms(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.mmem, Link.msub,Link.mprt} );
-  }
-  public List<Synset> getAllHolonyms(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.hmem,Link.hsub,Link.hprt} );
-  }
-  public List<Synset> getAllDomain(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.dmnc,Link.dmnu,Link.dmnr} );
-  }
-  public List<Synset> getAllMemberOfDomain(Synset synset) {
-    return getLinkedSynsets( synset, 
-            new Link[]{Link.dmtc,Link.dmtu,Link.dmtr} );
-  }
-  
-  //FIXME: before abstract wn release
-  boolean lenient = true;
-  
-  protected List<Synset> getLinkedSynsets( Synset synset, Link[] links ) {
-    List<Synset> retval = new ArrayList<Synset>();
+  public Map<Link,List<Synset>> getLinkedSynsetsAndWords( Synset synset, List<Link> links ) {
+    int s = links.size();
+    Map<Link,List<Synset>> retval = new LinkedHashMap<Link,List<Synset>>(s*4/3+1);
+    List<Link> synsetLinks = new ArrayList<Link>(s);
+    List<Link> wordLinks = new ArrayList<Link>(s);
     for ( Link link : links ) {
       if (link.isDefinedAmongSynsets()) {
-        retval.addAll( getLinkedSynsets( synset, link ) );
+        synsetLinks.add( link );
       }
-      if (link.isDefinedAmongWords() && (lenient || synset.getWord()!=null)) {
-        retval.addAll( getLinkedWords( synset.getSynsetId(), synset.getWord(), link ) );
-      }
-    }
-    Set<String> history = new HashSet<String>();
-    List<Synset> duplicated = new ArrayList<Synset>();
-    for ( Synset s : retval ) {
-      if (history.contains(s.getSynsetId())) {
-        duplicated.add(s);
-      } else {
-        history.add(s.getSynsetId());
+      //else if
+      if (link.isDefinedAmongWords()) {
+  //    if (link.isDefinedAmongWords() && (lenient || synset.getWord()!=null)) {
+        wordLinks.add( link );
       }
     }
-    retval.removeAll(duplicated);
+    if (synsetLinks.size()>0) {
+      retval.putAll( getLinkedSynsets( synset, synsetLinks ) );
+    }
+    if (wordLinks.size()>0) {
+      retval.putAll( getLinkedWords( synset.getSynsetId(), synset.getWord(), wordLinks ) );
+    }
     return retval;
   }
+  
+//  protected List<Synset> getLinkedSynsetsAndWords( Synset synset, Link[] links ) {
+//    List<Synset> retval = new ArrayList<Synset>();
+//    List<Link> synsetLinks = new ArrayList<Link>(links.length);
+//    List<Link> wordLinks = new ArrayList<Link>(links.length);
+//    for ( Link link : links ) {
+//      if (link.isDefinedAmongSynsets()) {
+//        synsetLinks.add( link );
+//      } else if (link.isDefinedAmongWords()) {
+////      if (link.isDefinedAmongWords() && (lenient || synset.getWord()!=null)) {
+//        wordLinks.add( link );
+//      }
+//    }
+//    {
+//      Map<Link,List<Synset>> linked = getLinkedSynsets( synset );
+//      for ( Link link : synsetLinks ) {
+//        List<Synset> synsets = linked.get(link);
+//        if (synsets!=null) {
+//          retval.addAll(synsets);
+//        }
+//      }
+//    }
+//    {
+//      Map<Link,List<Synset>> linked = getLinkedWords( synset.getSynsetId(), synset.getWord() );
+//      for ( Link link : wordLinks ) {
+//        List<Synset> synsets = linked.get(link);
+//        if (synsets!=null) {
+//          retval.addAll(synsets);
+//        }
+//      }
+//    }
+//    Set<String> history = new HashSet<String>();
+//    List<Synset> duplicated = new ArrayList<Synset>();
+//    for ( Synset s : retval ) {
+//      if (history.contains(s.getSynsetId())) {
+//        duplicated.add(s);
+//      } else {
+//        history.add(s.getSynsetId());
+//      }
+//    }
+//    retval.removeAll(duplicated);
+//    return retval;
+//  }
   
   /**
    * Given a synset id, get human-readable synset label e.g. "jogging#n#1"
@@ -244,20 +235,6 @@ public abstract class AbstractWordNet {
       }
     }
     return lemma+"#"+pos.toString()+"#"+num;
-    
-//    Below is wrong implementation
-//    
-//    String name = getNameOfSynset(synsetId);
-//    List<String> words = getWordLemmas(synsetId);
-//    int num = -1;
-//    for ( int i=0; i<words.size(); i++ ) {
-//      String w = words.get(i);
-//      if (w.equals(name)) {
-//        num = i+1;
-//        break;
-//      }
-//    }
-//    return name+"#"+getPOS(synsetId)+"#"+num;
   }
   
   /**
@@ -280,12 +257,64 @@ public abstract class AbstractWordNet {
       return null;
     }
   }
-  
-  /**
-   * (Optional method to implement)
-   *  
-   * @return all words (lemmas) available for look up in WordNet
-   */
-  public abstract Set<String> dumpWords();
 
+  public Synset getSynset( String wordLemma, POS pos, int senseId ) throws IllegalArgumentException {
+    List<Synset> synsets = getSynsets(wordLemma, pos);
+    if ( synsets==null ) throw new IllegalArgumentException(msgInvalidWord);
+    if ( synsets.size() < senseId ) throw new IllegalArgumentException(msgInvalidSense);
+    return synsets.get(senseId-1);
+  }
+
+  /**
+   * Input can be a word_lemma (cat), word_lamma+pos (cat#n), 
+   * word_lemma+pos+sense_num (cat#n#1). Not synsetId(00002039-n).
+   * 
+   * @param wps
+   * @return synsets
+   */
+  public List<Synset> getSynsetsFromWPS( String wps ) throws IllegalArgumentException {
+    List<Synset> retval = new ArrayList<Synset>();
+    if (wps==null || wps.trim().length()==0) throw new IllegalArgumentException(msgInvalidWord);
+    try {
+      String[] split = wps.split("#");
+      if ( split.length==3 ) {
+        String lemma = split[0];
+        POS pos = null; 
+        try {
+          pos = POS.valueOf(split[1]);
+        } catch (IllegalArgumentException e) {
+          throw new IllegalArgumentException(msgInvalidPOS);
+        }
+        int sense = Integer.parseInt(split[2]);
+        List<Synset> synsets = getSynsets(lemma, pos);
+        if (synsets.size()==0) throw new IllegalArgumentException(msgInvalidWord);
+        if (synsets.size()<sense) throw new IllegalArgumentException(msgInvalidSense);
+        retval.add(synsets.get(sense-1));
+        return retval;
+      } else if ( split.length==2 ) {
+        String lemma = split[0];
+        POS pos = null; 
+        try {
+          pos = POS.valueOf(split[1]);
+        } catch (IllegalArgumentException e) {
+          throw new IllegalArgumentException(msgInvalidPOS);
+        }
+        retval = getSynsets(lemma, pos);
+        if (retval.size()==0) throw new IllegalArgumentException(msgInvalidWord);
+        return retval;
+      } else if ( split.length==1 ) {
+        for ( POS pos : POS.values() ) {
+          retval.addAll(getSynsets(wps, pos));
+        }
+        if (retval.size()==0) throw new IllegalArgumentException(msgInvalidWord);
+        return retval;
+      }
+    } catch ( NumberFormatException e ) {
+      throw new IllegalArgumentException(msgInvalidSense);
+    } catch ( IndexOutOfBoundsException e ) {
+      throw new IllegalArgumentException(msgInvalidSense);
+    }
+    return retval;
+  }
+  
 }
---
+++ src/main/java/edu/cmu/lti/abstract_wordnet/LinkedSynsets.java
@@ -0,0 +1,115 @@
+package edu.cmu.lti.abstract_wordnet;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class LinkedSynsets extends LinkedHashMap<Link,List<Synset>> {
+
+  private static final long serialVersionUID = 1L;
+  
+  public static final Link[] horizLinks = new Link[]{Link.also, Link.attr, Link.sim, 
+          Link.ants, Link.pert};
+  public static final Link[] upLinks = new Link[]{Link.hype, Link.inst,//hypes 
+          Link.mmem, Link.msub,Link.mprt };//mero ;
+  public static final Link[] downLinks = 
+          new Link[]{Link.hmem, Link.hsub, Link.hprt,//holo 
+          Link.hypo, Link.hasi, //hypos
+          Link.caus, Link.enta, };//etc;
+  public static final Link[] hypeLinks = new Link[]{Link.hype, Link.inst};
+  public static final Link[] hypoLinks = new Link[]{Link.hypo, Link.hasi};
+  public static final Link[] meroLinks = new Link[]{Link.mmem, Link.msub, Link.mprt};
+  public static final Link[] holoLinks = new Link[]{Link.hmem, Link.hsub, Link.hprt};
+  public static final Link[] domLinks  = new Link[]{Link.dmnc, Link.dmnu, Link.dmnr};
+  public static final Link[] memLinks  = new Link[]{Link.dmtc, Link.dmtu, Link.dmtr};
+  
+  public LinkedSynsets( AbstractWordNet wn, Synset synset, List<Link> links ) {
+    super(wn.getLinkedSynsetsAndWords( synset, links ));
+  }
+
+  public LinkedSynsets( Map<Link,List<Synset>> linkedSynsets ) {
+    super(linkedSynsets);
+  }
+  
+  /**
+   * Given a synset id s, find synset(s) of  
+   * direct horizontal term(s) of s.
+   * 
+   * Such terms are linked with relations such as: ants, attr, sim.
+   * 
+   * @param synsetId
+   * @return synsets of direct hypernym or empty collection if N/A
+   */
+  public List<Synset> getHorizontals() {
+    return getLinkedSynsetsAndWords( horizLinks );
+  }
+  
+  /**
+   * Given a synset id s, find synset(s) of  
+   * direct upward term(s) of s.
+   * 
+   * Such terms are linked with relations such as: 
+   * hype, mero, mmem, mprt, msub
+   * 
+   * Used by HSO algorithm.
+   * 
+   * @param synset
+   * @return synsets of direct hypernym or empty collection if N/A
+   */
+  public List<Synset> getUpwards() {
+    return getLinkedSynsetsAndWords( upLinks );
+  }
+
+  /**
+   * Given a synset id s, find synset(s) of  
+   * direct downward term(s) of s.
+   * 
+   * Such terms are linked with relations such as: 
+   * caus, enta, holo, hmem, hsub, hprt, hypo.
+   * 
+   * Used by HSO algorithm.
+   * 
+   * @param synset
+   * @return synsets of direct hypernym or empty collection if N/A
+   */
+  public List<Synset> getDownwards() {
+    return getLinkedSynsetsAndWords( downLinks );
+  }
+  
+  public List<Synset> getHypes() {
+    return getLinkedSynsetsAndWords( hypeLinks );
+  }
+  
+  public List<Synset> getHypos() {
+    return getLinkedSynsetsAndWords( hypoLinks );
+  }
+  
+  public List<Synset> getAllMeronyms() {
+    return getLinkedSynsetsAndWords( meroLinks );
+  }
+  
+  public List<Synset> getAllHolonyms() {
+    return getLinkedSynsetsAndWords( holoLinks );
+  }
+  
+  public List<Synset> getAllDomain() {
+    return getLinkedSynsetsAndWords( domLinks );
+  }
+  
+  public List<Synset> getAllMemberOfDomain() {
+    return getLinkedSynsetsAndWords( memLinks );
+  }
+  
+  public List<Synset> getLinkedSynsetsAndWords( Link ... links ) {
+    List<Synset> retval = new ArrayList<Synset>();
+    for ( Link link : links ) {
+      List<Synset> synsets = get(link);
+      if (synsets!=null) {
+        retval.addAll(synsets);
+      }
+    }
+    return retval;
+  }
+}
