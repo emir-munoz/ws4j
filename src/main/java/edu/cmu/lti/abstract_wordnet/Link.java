4aa5cb64ca1f54cccaec19d07a7b1a0f42ae0459
hideki.shima@gmail.com <hideki.shima@gmail.com@755b6b0a-766d-1655-8634-96f16c0b9a82>
2013-05-23 16:28:30 +0000

    Link object is associated with supported POS

--- src/main/java/edu/cmu/lti/abstract_wordnet/Link.java
+++ src/main/java/edu/cmu/lti/abstract_wordnet/Link.java
@@ -36,38 +36,48 @@ import java.util.Set;
 public enum Link {
 	// Comments include description and the number of instances found in v0.9
 	
-	also("^"), //See also 2692
-	hype("@"), //Hypernyms 89089
-	inst("@i"), //Instances 8577 //Instance Hypernym
-	hypo("~"), //Hyponym   89089
-	hasi("~i"), //Has Instance 8577 //Instance Hyponym
-	mero("%"), //Meronyms 0
-	mmem("%m"), //Meronyms --- Member 12293
-	msub("%s"), //Meronyms --- Substance 979
-	mprt("%p"), //Meronyms --- Part 9097
-	holo("#"), //Holonyms 0
-	hmem("#m"), //Holonyms --- Member 12293
-	hsub("#s"), //Holonyms --- Substance 797
-	hprt("#p"), //Holonyms -- Part 9097
-	attr("="), //Attributes 1278
-	 sim("&"),  //Similar to 21386
-	enta("*"), //Entails 408
-	caus(">"), //Causes 220
-	dmnc("-c"), //Domain --- Category/TOPIC 6643 / (Domain of synset - TOPIC)
-	dmnu("-u"), //Domain --- Usage 967 / (Domain of synset - USAGE)
-	dmnr("-r"), //Domain --- Region 1345 / (Domain of synset - REGION)
-	dmtc(";c"), //In Domain --- Category 6643 / (Member of this domain - TOPIC)
-	dmtu(";u"), //In Domain --- Usage 967 / (Member of this domain - USAGE)
-	dmtr(";r"), //In Domain --- Region 1345 / (Member of this domain - REGION)
-	ants("!"), //Antonyms 0
-	vgrp("$"), //Verb Group
-  deri("+"), //Derivationally related form
-  part("<"), //Participle of verb
+	also("^", "Also See", POS.v, POS.a), //See also 2692
+	hype("@", "Hypernym", POS.n, POS.v), //Hypernyms 89089
+	inst("@i", "Instance hypernym", POS.n), //Instances 8577 //Instance Hypernym
+	hypo("~", "Hyponym", POS.n, POS.v), //Hyponym   89089
+	hasi("~i","Instance hyponym", POS.n), //Has Instance 8577 //Instance Hyponym
+	mero("%", "Meronym", POS.n), //Meronyms 0//FIXME
+	mmem("%m", "Member meronym", POS.n), //Meronyms --- Member 12293
+	msub("%s", "Substance meronym", POS.n), //Meronyms --- Substance 979
+	mprt("%p", "Part meronym", POS.n), //Meronyms --- Part 9097
+	holo("#", "Holonym", POS.n), //Holonyms 0//FIXME
+	hmem("#m", "Member holonym", POS.n), //Holonyms --- Member 12293
+	hsub("#s", "Substance holonym", POS.n), //Holonyms --- Substance 797
+	hprt("#p", "Part holonym", POS.n), //Holonyms -- Part 9097
+	attr("=", "Attribute", POS.n, POS.a), //Attributes 1278
+	 sim("&", "Similar To", POS.a),  //Similar to 21386
+	enta("*", "Entailment", POS.v), //Entails 408
+	caus(">", "Cause", POS.v), //Causes 220
+	dmnc("-c", "Domain of synset - TOPIC",  POS.n, POS.v, POS.a, POS.r), //Domain --- Category/TOPIC 6643 / (Domain of synset - TOPIC)
+	dmnu("-u", "Domain of synset - USAGE",  POS.n, POS.v, POS.a, POS.r), //Domain --- Usage 967 / (Domain of synset - USAGE)
+	dmnr("-r", "Domain of synset - REGION", POS.n, POS.v, POS.a, POS.r), //Domain --- Region 1345 / (Domain of synset - REGION)
+	dmtc(";c", "Member of this domain - TOPIC",  POS.n), //In Domain --- Category 6643 / (Member of this domain - TOPIC)
+	dmtu(";u", "Member of this domain - USAGE",  POS.n), //In Domain --- Usage 967 / (Member of this domain - USAGE)
+	dmtr(";r", "Member of this domain - REGION", POS.n), //In Domain --- Region 1345 / (Member of this domain - REGION)
+	ants("!", "Antonym", POS.n, POS.v, POS.a, POS.r), //Antonyms 0
+	vgrp("$", "Verb Group", POS.v), //Verb Group
+  deri("+", "Derivationally related form", POS.n, POS.v, POS.a, POS.r), //Derivationally related form
+  part("<", "Participle", POS.a), //Participle of verb
 	//defa("\\"), //Derived from adjective: finer-grained pert used in jwi
-	pert("\\"), //Pertainym (adj pertains to nouns)
-	syns(null), //(words sharing) the same synset
+	pert("\\", "Pertainym (pertains to nouns) / Derived from adjective", POS.a, POS.r), //Pertainym (adj pertains to nouns)
+	syns(null, null), //(words sharing) the same synset//FIXME: remove if unused
 	;
 	
+  private String symbol;
+  private String longName;
+  private Set<POS> acceptablePOS;
+  
+  Link( String symbol, String name, POS ... acceptablePOS ) {
+    this.symbol = symbol;
+    this.longName = name;
+    this.acceptablePOS = new LinkedHashSet<POS>(Arrays.asList(acceptablePOS));
+  }
+  
 	private static final Set<Link> wordLinks = 
 	        new LinkedHashSet<Link>(Arrays.asList(new Link[]{
         	  also, 
@@ -89,11 +99,25 @@ public enum Link {
 	  return synsetLinks.contains(this);
 	}
 	
-	private String symbol;
-	Link( String symbol ) {
-	  this.symbol = symbol;
+	/**
+	 * Check if the link is defined on a given POS.
+	 * When a null value is given, it returns true!
+	 * 
+	 * @param pos 
+	 *   Part-of-speech to check
+	 * @return whether a link is defined on the pos or not, or true if arg is null
+	 */
+	public boolean isDefinedOnPOS( POS pos ) {
+	  return pos==null || acceptablePOS.contains(pos);
 	}
+	
   public String getSymbol() {
     return symbol;
   }
+
+  public String getLongName() {
+    return longName;
+  }
+  
+  
 }
